2e6d907de609cdc431d7c2cf9bd20860a3590eb1
Matthew Callahan <terrafirma9@gmail.com>
2025-01-24 16:46:06 -0600

    Initial Commit

---
+++ FetchProject/FetchProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchProjectApp.swift
+//  FetchProject
+//
+//  Created by Matthew Callahan on 1/24/25.
+//
+
+import SwiftUI
+
+@main
+struct FetchProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FetchProjectTests/FetchProjectTests.swift
@@ -0,0 +1,17 @@
+//
+//  FetchProjectTests.swift
+//  FetchProjectTests
+//
+//  Created by Matthew Callahan on 1/24/25.
+//
+
+import Testing
+@testable import FetchProject
+
+struct FetchProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
